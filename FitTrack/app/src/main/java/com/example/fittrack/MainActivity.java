69c3bb6dfc5c6dbf366b629654ac3d9410cbd8eb
jewell-owen <jewell.owen@yahoo.com>
2024-10-11 14:54:46 -0500

    set up nav bar function in main activity to handle switching between fragment

--- FitTrack/app/src/main/java/com/example/fittrack/MainActivity.java
+++ FitTrack/app/src/main/java/com/example/fittrack/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.fittrack;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    protected BottomNavigationView bottomNavigationView;
+    ProfileFragment profileFragment = new ProfileFragment();
+    FriendsFragment friendsFragment = new FriendsFragment();
+    WorkoutFragment workoutFragment = new WorkoutFragment();
+    StatsFragment statsFragment = new StatsFragment();
+    NutritionFragment nutritionFragment = new NutritionFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +31,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.Profile);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if ((item.getItemId() == R.id.Profile)) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, profileFragment)
+                    .commit();
+            return true;
+        } else if ((item.getItemId() == R.id.Friends)) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, friendsFragment)
+                    .commit();
+            return true;
+        } else if ((item.getItemId() == R.id.Workout)) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, workoutFragment)
+                    .commit();
+            return true;
+        } else if ((item.getItemId() == R.id.Stats)) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, statsFragment)
+                    .commit();
+            return true;
+        } else if ((item.getItemId() == R.id.Nutrition)) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, nutritionFragment)
+                    .commit();
+            return true;
+        } else {
+            return false;
+        }
     }
 }
