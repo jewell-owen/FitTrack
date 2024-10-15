cfffff13476be0d0a06d50b5f23fc92eb1fb8daf
jewell-owen <jewell.owen@yahoo.com>
2024-10-15 13:01:31 -0500

    set up layout changes for when user starts a freestyle workout, and added start of methods to add exercise cards. Currently it only handles adding an empty exercise card because we need to set up exercise API

--- FitTrack/app/src/main/java/com/example/fittrack/WorkoutFragment.java
+++ FitTrack/app/src/main/java/com/example/fittrack/WorkoutFragment.java
@@ -3,13 +3,18 @@ package com.example.fittrack;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +29,15 @@ public class WorkoutFragment extends Fragment implements View.OnClickListener{
     private ImageButton startWorkoutButton;
     private Button myWorkoutsButton;
     private Button plannedWorkoutButton;
+    private ImageButton finishWorkoutButton;
+    private ImageButton cancelWorkoutButton;
+    private HorizontalScrollView exerciseScrollView;
+    private Button resetButton;
+    private TextView timerTextView;
+    private Button startStopButton;
+    private ConstraintLayout restTimerLayout;
+    private LinearLayout cardLayout;
+    private TextView titleTextView;
 
 
     private String mParam1;
@@ -76,13 +90,38 @@ public class WorkoutFragment extends Fragment implements View.OnClickListener{
         startWorkoutButton = view.findViewById(R.id.workout_start_workout_btn);
         plannedWorkoutButton = view.findViewById(R.id.workout_add_plan_btn);
         myWorkoutsButton = view.findViewById(R.id.workout_my_workouts_btn);
+        finishWorkoutButton = view.findViewById(R.id.workout_finish_workout_btn);
+        cancelWorkoutButton = view.findViewById(R.id.workout_cancel_workout_btn);
+        exerciseScrollView = view.findViewById(R.id.workout_scroll_view);
+        resetButton = view.findViewById(R.id.btnReset);
+        timerTextView = view.findViewById(R.id.workout_timer_tv);
+        startStopButton = view.findViewById(R.id.btnStartStop);
+        restTimerLayout = view.findViewById(R.id.workout_rest_timer_cl);
+        titleTextView = view.findViewById(R.id.workout_title_tv);
+        cardLayout = view.findViewById(R.id.workout_card_ll);
 
 
         startWorkoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                if (plannedWorkout != null){
+//                    titleTextView.setText(plannedWorkout.getName());
+//                }
+//                else {
+//                    titleTextView.setText("Freestyle");
+//                }
                 plannedWorkoutButton.setVisibility(View.GONE);
-                plannedWorkoutButton.setVisibility(View.GONE);
+                myWorkoutsButton.setVisibility(View.GONE);
+                startWorkoutButton.setVisibility(View.GONE);
+                finishWorkoutButton.setVisibility(View.VISIBLE);
+                cancelWorkoutButton.setVisibility(View.VISIBLE);
+                exerciseScrollView.setVisibility(View.VISIBLE);
+                restTimerLayout.setVisibility(View.VISIBLE);
+                timerTextView.setVisibility(View.VISIBLE);
+
+                createCards();
+
+
             }
         });
 
@@ -102,5 +141,30 @@ public class WorkoutFragment extends Fragment implements View.OnClickListener{
         });
 
         return view;
+    }
+
+    private void createCards() {
+        // Create a new CardView for each card
+        CardView cardView = createCardView();
+        cardLayout.addView(cardView);
+    }
+
+    private CardView createCardView()  {
+        // Inflate the CardView layout
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        CardView cardView = (CardView) inflater.inflate(R.layout.card_empty_exercise, cardLayout, false);
+
+
+        // Set values to the views in the CardView
+        ImageButton btnCard = cardView.findViewById(R.id.card_select_exercise_btn);
+
+        btnCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return cardView;
     }
 }
