cd7cef73d53e970e9fb3f26db7a812540540135a
jewell-owen <jewell.owen@yahoo.com>
2024-10-15 11:35:26 -0500

    Started work on workout fragment, forgot to switch to branch swithcing now

--- FitTrack/app/src/main/java/com/example/fittrack/WorkoutFragment.java
+++ FitTrack/app/src/main/java/com/example/fittrack/WorkoutFragment.java
@@ -1,5 +1,6 @@
 package com.example.fittrack;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,20 +8,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link WorkoutFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WorkoutFragment extends Fragment {
+public class WorkoutFragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private ImageButton startWorkoutButton;
+    private Button myWorkoutsButton;
+    private Button plannedWorkoutButton;
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -36,7 +41,6 @@ public class WorkoutFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment WorkoutFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static WorkoutFragment newInstance(String param1, String param2) {
         WorkoutFragment fragment = new WorkoutFragment();
         Bundle args = new Bundle();
@@ -44,6 +48,11 @@ public class WorkoutFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
     }
 
     @Override
@@ -56,9 +65,42 @@ public class WorkoutFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        View view = inflater.inflate(R.layout.fragment_workout, container, false);
+        startWorkoutButton = view.findViewById(R.id.workout_start_workout_btn);
+        plannedWorkoutButton = view.findViewById(R.id.workout_add_plan_btn);
+        myWorkoutsButton = view.findViewById(R.id.workout_my_workouts_btn);
+
+
+        startWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                plannedWorkoutButton.setVisibility(View.GONE);
+                plannedWorkoutButton.setVisibility(View.GONE);
+            }
+        });
+
+        plannedWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        });
+
+        myWorkoutsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
     }
 }
